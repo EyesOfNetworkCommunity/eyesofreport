5f29be824fffecb67edf6eccadee0323bcf028db
Benoit Village <benoit.village@gmail.com>
2017-11-20 16:24:52 +0100

    update transfert scheduling script

--- SOURCES/build_docker_source/centos_ssh/source_transfert_scheduling_silent.sh
+++ SOURCES/build_docker_source/centos_ssh/source_transfert_scheduling_silent.sh
@@ -88,10 +88,10 @@ fi
 	######################## CONFIGURATION LOG NAGIOS TRANSFERT MANUAL ############################	
 	if [ $configure_nagios_source -eq 1 ]; then
 		
-		transfert_nagios_command="/usr/bin/rsync -avr --rsh=\"/usr/bin/ssh -p \$2 \" \`/usr/bin/find $nagios_log_path -type f -name nagios-\$4-03-\$3-00.log \`  eyesofreport@\$1:/home/eyesofreport/external_depot/Log_Nagios_Depot/"
+		transfert_nagios_command="/usr/bin/rsync -avr --rsh=\"/usr/bin/ssh -p \$2 \" \`/usr/bin/find $nagios_log_path -type f -name nagios-\$4-*-\$3-00.log \`  eyesofreport@\$1:/home/eyesofreport/external_depot/Log_Nagios_Depot/"
 
 		echo "$transfert_nagios_command" > $source_path/transfert_nagios_log_manual.sh
-		chmod +x $source_path/transfert_nagios_log.sh
+		chmod +x $source_path/transfert_nagios_log_manual.sh
 		chown -R eyesofreport:wheel $source_path
 	fi
 	
