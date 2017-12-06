131be7a4991dc758091c6363cf407235091042e5
Vincent FRICOU <vincent+github@fricouv.eu>
2017-12-06 16:22:23 +0100

    Update script to allow space and / in contract name.

--- ETL/Host_ETL_Env/scripts/script_generation_rapport_contrat_manuel.sh
+++ ETL/Host_ETL_Env/scripts/script_generation_rapport_contrat_manuel.sh
@@ -40,11 +40,15 @@ MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT distinct dcc_name from d_contract_c
 #Get contract id for contract "Full"
 #contract_context_id=$(MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT dcc_id from d_contract_context where dcc_name = 'Platinium_Application'" eor_dwh | tail -n +2)
 
-while read contrat
-do 
+nb_contrats=$(cat ${month_folder}/liste_contrats.txt | wc -l)
+for i in $(seq 1 ${nb_contrats})
+do
+
+	line_contrat="$(cat ${month_folder}/liste_contrats.txt | head -${i} | tail -1)"
+    contrat=$line_contrat
 	contrat_id=$(MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT dcc_id from d_contract_context where dcc_name='$contrat'" eor_dwh | tail -n +2)
 	#application_level=$(MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT dap_priority from d_application where dap_name='$application'" eor_dwh | tail -n +2)
-	
+	contrat=$(echo $line_contrat|sed -e 's/ /_/g' -e 's/\//-/g')
 	continue=1
 	#contract_context_id_custom="0"
 	#contract_context_name_custom="0"
@@ -53,17 +57,13 @@ do
 	cd $month_application_folder
 	
 	if [ $continue -eq 1 ]; then
-	
-			report_name=${date_month_folder}_${contrat}.pptx
-      rm -f ${date_month_folder}_${contrat}.pptx
-      
-			echo $(date +"%Y-%m-%d %r") "Generation rapport $contrat pour le $previous_year-$previous_month" >> $month_folder/creation_rapports.log
-      
-      
-			wget -q -O $report_name "http://localhost:8080/birt/run?__report=EOR_Contract_Application_FR.rptdesign&__format=PPTX&Year=$previous_year&__isdisplay__Year=$previous_year&Month=$previous_month&__isdisplay__Month=$previous_month&contract_context=$contrat" >> /dev/null
-      
+		report_name=${date_month_folder}_${contrat}.pptx
+		rm -f ${date_month_folder}_${contrat}.pptx
+		contrat=$line_contrat
+		echo $(date +"%Y-%m-%d %r") "Generation rapport $contrat pour le $previous_year-$previous_month" >> $month_folder/creation_rapports.log
+		wget -q -O $report_name "http://localhost:8080/birt/run?__report=EOR_Contract_Application_FR.rptdesign&__format=PPTX&Year=$previous_year&__isdisplay__Year=$previous_year&Month=$previous_month&__isdisplay__Month=$previous_month&contract_context=$contrat" >> /dev/null
 	fi
-done < $month_folder/liste_contrats.txt
+done
 
 cd $month_folder
 
