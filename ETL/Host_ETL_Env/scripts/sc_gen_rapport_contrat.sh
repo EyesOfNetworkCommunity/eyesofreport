304d609beffb604cdaa411c6239553e7649d6c4b
Vincent FRICOU <vincent+github@fricouv.eu>
2017-12-07 10:40:30 +0100

    Improvement on script for generate contrat report
    * Merge two previous scripts
      * script_generation_rapport_contrat.sh
      * script_generation_rapport_contrat_manuel.sh
    * Deleting previous scripts
    * Add new script for merged with help and toggles to use it in manual mode or not

--- ETL/Host_ETL_Env/scripts/sc_gen_rapport_contrat.sh
+++ ETL/Host_ETL_Env/scripts/sc_gen_rapport_contrat.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
- echo "Usage script_generation_rapport.sh year month"
- exit 1
-fi
+SCRIPTNAME="sc_gen_rapport_contrat.sh"
+REVISION="0.1"
 
-if [ -z "$2" ]; then
- echo "Usage script_generation_rapport.sh year month"
- exit 1
-fi
+usage () {
+echo "Usage : ${SCRIPTNAME} - Version : ${REVISION}
+This script writen to permit report generation automaticaly for daily gen in crontab or
+manually to generate global month.
 
+Options :
+	-h             Invoke this help
+	-M             Force script to manual usage (You could specify -y and -m)
+	-y <year>      Specify year for generate contrat report (Format YYYY)
+	-m <month>     Specify month for generate contrat report (Format MM)"
+exit 3
+}
+
+ARGS="$(echo $@ |sed -e 's:-[[:alpha:]] :\n&:g' | sed -e 's: ::g')"
+for i in $ARGS; do
+        if [ -n "$(echo ${i} | grep "^\-h")" ]; then usage ;fi
+        if [ -n "$(echo ${i} | grep "^\-M")" ]; then Mode="manual" ;fi
+        if [ -n "$(echo ${i} | grep "^\-y")" ]; then previous_year="$(echo ${i} | cut -c 3-)"; fi
+        if [ -n "$(echo ${i} | grep "^\-m")" ]; then previous_month="$(echo ${i} | cut -c 3-)"; fi
+done
+
+if [ ! ${Mode} ]
+then
+	previous_year=$(date --date="$(date +%Y-%m-15)" +'%Y')
+	previous_month=$(date --date="$(date +%Y-%m-15)" +'%Y')
+	date_month_folder=${previous_year}${previous_month}
+	date_month=${previous_year}${previous_month}-01
+elif [ ${Mode} == "manual" ]
+then
+	if [ ! ${previous_year} ];then echo "You've not specify year" ; exit 3 ;fi
+	if [ ! ${previous_month} ];then echo "You've not specify month"; exit 3 ;fi
+	date_month_folder=${previous_year}${previous_month}
+	date_month=${previous_year}${previous_month}-01
+else
+	echo "Invalid mode"
+	exit 3
+fi
 
 MYSQL_PWD=root66
-previous_year=$1
-previous_month=$2
-#previous_year=$(date --date="$(date +%Y-%m-15)" +'%Y')
-#previous_month=$(date --date="$(date +%Y-%m-15)" +'%m')
-date_month_folder=${previous_year}${previous_month}
-date_month=${previous_year}-${previous_month}-01
 month_folder=/var/archive/eyesofreport/Contract_Monthly/${date_month_folder}
-
-
 
 mkdir -p $month_folder
 
 current_datetime=$(date +"%Y-%m-%d %r")
 echo $current_datetime "Creation du dossier /var/archive/eyesofreport/Contract_Monthly/"$date_month " OK" >> $month_folder/creation_rapports.log
 
-#application_list=$(MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT distinct dap_name from d_application" eor_dwh)
-
 application_list=$(MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT distinct dcc_name from d_contract_context" eor_dwh)
 
-#MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT distinct dap_name from d_application where dap_priority=1" eor_dwh | tail -n +2 > $month_folder/liste_application.txt
-
 MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT distinct dcc_name from d_contract_context" eor_dwh | tail -n +2 > $month_folder/liste_contrats.txt
-
-#Get contract id for contract "Full"
-#contract_context_id=$(MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT dcc_id from d_contract_context where dcc_name = 'Platinium_Application'" eor_dwh | tail -n +2)
 
 nb_contrats=$(cat ${month_folder}/liste_contrats.txt | wc -l)
 for i in $(seq 1 ${nb_contrats})
@@ -45,11 +60,8 @@ do
 	line_contrat="$(cat ${month_folder}/liste_contrats.txt | head -${i} | tail -1)"
     contrat=$line_contrat
 	contrat_id=$(MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT dcc_id from d_contract_context where dcc_name='$contrat'" eor_dwh | tail -n +2)
-	#application_level=$(MYSQL_PWD=$MYSQL_PWD mysql -uroot -e "SELECT dap_priority from d_application where dap_name='$application'" eor_dwh | tail -n +2)
 	contrat=$(echo $line_contrat|sed -e 's/ /_/g' -e 's/\//-/g')
 	continue=1
-	#contract_context_id_custom="0"
-	#contract_context_name_custom="0"
 	month_application_folder=$month_folder/$contrat
 	mkdir -p $month_application_folder
 	cd $month_application_folder
