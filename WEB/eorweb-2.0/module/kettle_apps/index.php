706c225ad7fecaed47e4336bbc210ba879e043e1
jeanphilippe.levy <jeanphilippe.levy@gmail.com>
2018-02-19 14:42:04 +0100

    Fix kettle_apps loader

--- WEB/eorweb-2.0/module/kettle_apps/index.php
+++ WEB/eorweb-2.0/module/kettle_apps/index.php
@@ -1,87 +1,61 @@
-<?php 
+<?php
+/*
+#########################################
+#
+# Copyright (C) 2018 EyesOfNetwork Team
+# DEV NAME : Jean-Philippe LEVY
+# VERSION : 2.0
+# APPLICATION : eorweb for eyesofreport project
+#
+# LICENCE :
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#########################################
+*/
+
 include ("../../header.php"); 
 include("../../side.php");
 ?>
 
 <div id="page-wrapper">
 
-<link rel="stylesheet" type="text/css" href="../../bower_components/bootstrap-daterangepicker/daterangepicker.css" />
-
-<div class="row">
-	<div class="col-lg-12">
-		<h1 class="page-header marge"><?php echo getLabel("label.kettle_apps.title"); ?></h1>
+	<div class="row">
+		<div class="col-lg-12">
+			<h1 class="page-header marge"><?php echo getLabel("label.kettle_apps.title"); ?></h1>
+		</div>
 	</div>
-</div>
 
-<div id="message"></div>
+	<div id="message"></div>
 
-<form id="global">
-	<div class="row form-group">
-		<label class="col-md-3"><?php echo getLabel("label.kettle_apps.time_period_select"); ?></label>
-		<div class="col-md-9 input-validity-date">
-			<div class="form-group input-group">
-				<input type="text" class="form-control" readonly id="validity_date">
-				<span class="input-group-addon">
-					<span class="glyphicon glyphicon-calendar"></span>
-				</span>
+	<form id="global">
+		<div class="row form-group">
+			<label class="col-md-3" style="font-weight: normal"><?php echo getLabel("label.kettle_apps.time_period_select"); ?></p></label>
+			<div class="col-md-9 input-validity-date">
+				<div class="form-group input-group">
+					<input type="text" class="form-control" readonly id="validity_date">
+					<span class="input-group-addon">
+						<span class="glyphicon glyphicon-calendar"></span>
+					</span>
+				</div>
+			</div>
+			<div id="startDate"></div>
+			<div id="endDate"></div>
+		</div>
+		<div class="row form-group">
+			<div class="col-md-3">
+				<button class="form-group btn btn-primary" type="submit" id="launch"><?php echo getLabel("label.kettle_apps.launch"); ?>
+				</button>
 			</div>
 		</div>
-		<div id="startDate"></div>
-		<div id="endDate"></div>
-	</div>
-	<div class="row form-group">
-		<div class="col-md-3">
-			<button class="form-group btn btn-primary" type="submit" id="launch"><?php echo getLabel("label.kettle_apps.launch"); ?>
-			</button>
-		</div>
-	</div>
-</form>
+	</form>
 
 </div>
 
-<!-- jQuery -->
-<script src="/bower_components/jquery/dist/jquery.min.js"></script>
-
-<script>
-	$(document).ready(function() {
-		$('#validity_date').daterangepicker(
-		{
-			dateLimit: {
-				days: 30
-			},
-		    locale: {
-		    	firstDay: 1,
-		    	format: 'DD/MM/YYYY'
-		    }
-		}, function(start, end) {
-			if ( (start.format('MM') == end.format('MM')) && (start.format('YYYY') == end.format('YYYY')) ){
-				document.getElementById('message').innerHTML = "";
-				document.getElementById('launch').removeAttribute("disabled");
-				$('#startDate').val(start.format('YYYYMMDD'));
-				$('#endDate').val(end.format('YYYYMMDD'));
-			} else {
-				document.getElementById('message').innerHTML = "<?php message(0, "selectionnez deux dates du meme mois", "critical"); ?>";
-				document.getElementById('launch').setAttribute("disabled", true);
-			}
-		});
-		$('#launch').click(function(event){
-			event.preventDefault();
-			$.get(
-				'./php/get_kettle_id.php',
-				{
-					begin_date: $('#startDate').val(),
-					end_date: $('#endDate').val()
-				},
-				function ReturnID(id){
-					$(location).attr('href', 'http://<?php echo $_SERVER['SERVER_NAME']."/module/module_frame/index.php?url=".urlencode("/kettle/jobStatus/?name=JOB_MAIN_DATE&id=")?>' + id);	
-				},
-				'text'
-			);
-		});
-	});
-	
-</script>
-
-<?php
-include("./footer.php"); 
-?>
+<?php include("../../footer.php"); ?>
