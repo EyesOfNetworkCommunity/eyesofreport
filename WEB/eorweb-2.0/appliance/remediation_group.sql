11dfb91b700fd8f5a78b88ddee9c0a9b88a3fbaf
maxtkl <maxime287@hotmail.com>
2018-07-12 13:52:02 +0200

    fix remediation_group.sql (special character)

--- WEB/eorweb-2.0/appliance/remediation_group.sql
+++ WEB/eorweb-2.0/appliance/remediation_group.sql
@@ -1,7 +1,9 @@
+DROP TABLE IF EXISTS `remediation_group`;
 CREATE TABLE `remediation_group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+)
+COLLATE='latin1_swedish_ci';
 
 ALTER TABLE remediation_action ADD COLUMN id_group int(11) DEFAULT NULL;
